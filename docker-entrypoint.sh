a3deba4b9dd03b7209d3709ca03fc20f52c0cb5e
Adam H Griffith <adam.h.griffith@utah.edu>
2022-02-02 21:07:22 -0700

    Minor tweak to docker-entrypoint.sh to match upstream repo.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 
 # Set up and load test data:
 cd /usr/src/app
-python ./manage.py initial_setup
-python ./manage.py load_test_data
+python3 ./manage.py initial_setup
+python3 ./manage.py load_test_data
 
 # Run web server:
 python ./manage.py runserver 0.0.0.0:8000
